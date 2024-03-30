1c3c016f8e00c6acf8bd1a31f984ae4840357f29
abdelali djebara <adjebara@sqli.com>
2024-03-30 14:16:13 +0000

    form wrapper

--- src/app/components/form-wrapper/form-wrapper.component.ts
+++ src/app/components/form-wrapper/form-wrapper.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { PersonalDetailsComponent } from '../personal-details/personal-details.component';
+import { ExperienceListComponent } from '../experience/experience-list/experience-list.component';
+import { EducationListComponent } from '../education/education-list/education-list.component';
 
 @Component({
   selector: 'rb-form-wrapper',
   standalone: true,
-  imports: [],
+  imports: [
+    PersonalDetailsComponent,
+    ExperienceListComponent,
+    EducationListComponent,
+  ],
   templateUrl: './form-wrapper.component.html',
-  styleUrl: './form-wrapper.component.scss'
+  styleUrl: './form-wrapper.component.scss',
 })
-export class FormWrapperComponent {
-
-}
+export class FormWrapperComponent {}
