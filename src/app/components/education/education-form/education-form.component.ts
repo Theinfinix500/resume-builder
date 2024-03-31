b072dcaa3d755cd02efb49d7e3ad5fa286a1451e
abdelali djebara <adjebara@sqli.com>
2024-03-31 16:05:52 +0000

    education form changes

--- src/app/components/education/education-form/education-form.component.ts
+++ src/app/components/education/education-form/education-form.component.ts
@@ -4,11 +4,19 @@ import { Observable } from 'rxjs';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { AsyncPipe } from '@angular/common';
 import { NgxsFormPluginModule } from '@ngxs/form-plugin';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'rb-education-form',
   standalone: true,
-  imports: [ReactiveFormsModule, AsyncPipe, NgxsFormPluginModule],
+  imports: [
+    ReactiveFormsModule,
+    AsyncPipe,
+    NgxsFormPluginModule,
+    InputTextModule,
+    ButtonModule,
+  ],
   templateUrl: './education-form.component.html',
   styleUrl: './education-form.component.scss',
   encapsulation: ViewEncapsulation.None,
